a982ac29d2f81c4e532559321376974047f6d32d
chazzels <chazzels22@gmail.com>
2018-03-29 05:47:42 +0000

    added connection status monitor with connection re-attempt

--- js/fixture.client/websocketClient.js
+++ js/fixture.client/websocketClient.js
@@ -1,17 +1,59 @@
 "use strict";
 class WebSocketClient {
-    constructor(serverAdress) {
+    constructor(targetAddress) {
         this.WebSocket = require("ws");
+        this.serverAddress = "ws://127.0.0.1:80/";
+        this.connected = false;
+        this.attempting = false;
+        this.timerActive = false;
         let that = this;
         console.log("SOCKET_CLIENT::STARTING");
-        console.log("SOCKET_CLIENT::CONNECTING", serverAdress.toString());
-        this.ws = new this.WebSocket(serverAdress.toString());
-        this.ws.on('open', function socketOpen() {
-            that.ws.send("meta-stuff");
-        });
+        if (targetAddress !== undefined && targetAddress !== null) {
+            this.serverAddress = targetAddress;
+        }
+        this.attemptConnection();
     }
-    sendMeta() {
-        this.ws.send("meta-stuff");
+    startMonitor(that) {
+        if (that.timerActive === false) {
+            console.log("SOCKET_CLIENT::MONITOR_STARTING");
+            this.statusMonitorTimer(that);
+        }
+        else {
+            console.log("SOCKET_CLIENT::MONITOR_RUNNING");
+        }
+    }
+    statusMonitorTimer(that) {
+        setTimeout(function monitorTick() {
+            that.statusMonitorLoop(that);
+            that.statusMonitorTimer(that);
+        }, 5000, that);
+    }
+    statusMonitorLoop(that) {
+        if (this.connected === false && this.attempting === false) {
+            that.attemptConnection(that.serverAddress);
+        }
+        else if (this.connected === true && this.attempting === false) {
+        }
+    }
+    attemptConnection() {
+        let that = this;
+        that.attempting = true;
+        if (that.connected === false) {
+            console.log("SOCKET_CLIENT::CONNECTING", that.serverAddress.toString());
+            this.ws = new that.WebSocket(that.serverAddress.toString());
+            this.ws.on('open', function socketOpen() {
+                that.attempting = false;
+                that.connected = true;
+                that.startMonitor(that);
+            });
+            this.ws.on('error', function socketError(error) {
+                console.log("SOCKET_SERver::CONNECTION_ERROR:" + error.code);
+                console.error(error);
+                that.attempting = false;
+                that.connected = false;
+                that.startMonitor(that);
+            });
+        }
     }
 }
 module.exports = WebSocketClient;
--- src/fixture.client/websocketClient.ts
+++ src/fixture.client/websocketClient.ts
@@ -2,28 +2,106 @@ class WebSocketClient {
 	
 	private WebSocket: any = require("ws");
 	private ws: any;
+	private serverAddress: string = "ws://127.0.0.1:80/";
+	private connected: boolean = false;
+	private attempting: boolean = false;
+	private timerActive: boolean = false;
 	
-	constructor(serverAdress: string) {
+	constructor(targetAddress: string) {
 		
 		let that = this;
 		
 		console.log("SOCKET_CLIENT::STARTING");
 		
-		console.log("SOCKET_CLIENT::CONNECTING", serverAdress.toString());
-		
-		this.ws = new this.WebSocket(serverAdress.toString());
-		
-		this.ws.on('open', function socketOpen() {
+		if(targetAddress !== undefined && targetAddress !== null) {
 			
-			that.ws.send("meta-stuff");
+			this.serverAddress = targetAddress;
 			
-		});
+		}
+		
+		this.attemptConnection();
 		
 	}
 	
-	private sendMeta() {
+	private startMonitor(that) {
 		
-		this.ws.send("meta-stuff");
+		if(that.timerActive === false) { 
+			
+			console.log("SOCKET_CLIENT::MONITOR_STARTING");
+			
+			this.statusMonitorTimer(that);
+			
+		} else {
+			
+			console.log("SOCKET_CLIENT::MONITOR_RUNNING");
+			
+		}
+		
+	}
+	
+	private statusMonitorTimer(that) {
+		
+		setTimeout(function monitorTick() {
+			
+			that.statusMonitorLoop(that);
+			
+			that.statusMonitorTimer(that);
+			
+		}, 5000, that);
+		
+	}
+	
+	private statusMonitorLoop(that) {
+		
+		if(this.connected === false && this.attempting === false) {
+			
+			that.attemptConnection(that.serverAddress);
+			
+		} else if(this.connected === true && this.attempting === false) {
+			
+			//console.log("SOCKET_CLIENT::MONITOR_CHECK: PASS");
+			
+		}
+		
+	}
+	
+	private attemptConnection() {
+		
+		let that = this;
+		
+		that.attempting = true;
+		
+		if(that.connected === false) {
+			
+			console.log("SOCKET_CLIENT::CONNECTING", that.serverAddress.toString());
+			
+			this.ws = new that.WebSocket(that.serverAddress.toString());
+			
+			this.ws.on('open', function socketOpen() {
+				
+				that.attempting = false;
+				
+				that.connected = true;
+				
+				that.startMonitor(that);
+				
+			});
+			
+			this.ws.on('error', function socketError(error: any) {
+				
+				console.log("SOCKET_SERver::CONNECTION_ERROR:" + error.code);
+				
+				console.error(error);
+				
+				that.attempting = false;
+				
+				that.connected = false;
+				
+				that.startMonitor(that);
+				
+			});
+		
+		}
 		
 	}
 	
