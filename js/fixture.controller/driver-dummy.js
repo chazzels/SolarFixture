f83234d59fe492da76b59f5be89c28a1e53081a7
chazzels <chazzels22@gmail.com>
2018-05-11 17:39:18 +0000

    removing compiled/built files from being tracked as this could cause issue and adds extra work to manage generated files.

--- js/fixture.controller/driver-dummy.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class fixtureDummyDriver {
-    constructor() {
-    }
-    updateChannel() {
-    }
-}
-module.exports = fixtureDummyDriver;
-//# sourceMappingURL=driver-dummy.js.map
