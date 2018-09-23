6e23f7b0177dfb2125d5e343a6a9c2ea4aa84a93
chazzels <chazzels22@gmail.com>
2018-09-23 05:01:54 +0000

    setting correct server address for development environment

--- dev/dev.js
+++ dev/dev.js
@@ -2,7 +2,7 @@ let SolarFixture = require("../js/solarFixture");
 let fixture = new SolarFixture({
 	perf: {},
 	client: {
-		serverAddress: "ws://192.168.4.40"
+		serverAddress: "ws://0.0.0.0:8080"
 	},
 	controller: {
 		driver: "dummy"
